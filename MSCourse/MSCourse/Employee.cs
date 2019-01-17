5bc5b94723371ba6e352ff89c5acbd2609860f73
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-17 15:34:03 -0500

    inheritance demo

---
+++ MSCourse/MSCourse/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace MSCourse
+{
+    public class Employee
+    {
+        private string empNumber;
+        private string firstName;
+        private string lastName;
+        private string address;
+
+        public Employee()
+        {
+        }
+
+        public string EmpNumber
+        {
+            get
+            {
+                return empNumber;
+            }
+            set
+            {
+                empNumber = value;
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+    }
+}
