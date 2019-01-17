21dc685c115b507ed3f03b11cc65de33a469651a
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-17 11:04:38 -0500

    intro to OOP

---
+++ MSCourse/MSCourse/DrinksMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace MSCourse
+{
+    public class DrinksMachine
+    {
+        private string _location;
+        public string Location
+        {
+            get
+            {
+                return _location;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _location = value;
+                }
+            }
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Age { get; set; }
+
+        public void MakeCappuccino()
+        {
+
+        }
+
+        public void MakeEspresso()
+        {
+            Console.WriteLine("Cappuccino is made.");
+        }
+
+
+        //public event OutOfBeansHandler OutOfBeans;
+
+        public DrinksMachine()
+        {
+
+    }
+    }
+
+    //public partial class DrinksMachine
+    //{
+
+    //}
+}
---
+++ MSCourse/MSCourse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MSCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrinksMachine dm = new DrinksMachine();
+
+            //use type inference, compile does not know type before initialization
+            var dm2 = new DrinksMachine();
+            dm.Make = "Fourth Coffee";
+            dm.Model = "Beancrusher 3000";
+            dm.Age = 2;
+            dm.MakeEspresso();
+
+            Console.WriteLine(dm.Location);
+
+        }
+    }
+}
