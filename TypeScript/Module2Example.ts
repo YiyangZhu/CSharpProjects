016b46bd29c69d65f56dbb6379202afe7e80e2b7
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-20 15:50:20 -0500

    example

--- TypeScript/Module2Example.ts
+++ TypeScript/Module2Example.ts
@@ -1,3 +1,12 @@
 let someString : string;
-let someNumber: number;
-let 
+let someNumber : number;
+let trueOrFalse : boolean;
+let someArray : Array<any>;
+let someElement : Element;
+let someObject : object; // wrap Json object
+let nullThing: null;
+let undefinedThing: undefined;
+let voidThing : void;
+
+let anyoldthing : any;
+
