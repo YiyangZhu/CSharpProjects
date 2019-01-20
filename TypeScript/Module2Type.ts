7431be098ef1861165bcc2c73227db37a562093c
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-20 15:42:02 -0500

    array and map

---
+++ TypeScript/Module2Type.ts
@@ -0,0 +1,17 @@
+let numArray = [
+    1,
+    2,
+    'interruption cow',
+    {
+        index:3,
+        title:'interrupting cow'
+    }
+]
+
+let add1 = (num) => {
+    return 1 + num;
+}
+
+numArray.map((item) => {
+    alert(add1(item));
+})
