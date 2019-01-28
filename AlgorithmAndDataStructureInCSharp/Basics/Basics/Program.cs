44ecbd090f0d7ee8a914369bb10beddc990d5a07
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-28 15:58:32 -0500

    basic

---
+++ AlgorithmAndDataStructureInCSharp/Basics/Basics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numBase = 2;
+            int exp = 16;
+            int result = 1;
+            while(exp > 0)
+            {
+                result *= numBase;
+                exp--;
+            }
+            Console.WriteLine($"Result is {result}");
+        }
+    }
+}
