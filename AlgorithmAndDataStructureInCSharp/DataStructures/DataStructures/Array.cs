95df1d495e4824e1c3615966edae4bf93bf72607
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-30 11:00:02 -0500

    rearrange

---
+++ AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/Array.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DataStructures
+{
+    public class Array
+    {
+        public Array()
+        {
+        }
+
+        public static void TypeArrayDemo()
+        {
+            Type[][] jaggedArray = new Type[10][];
+            jaggedArray[0] = new Type[5];
+            jaggedArray[1] = new Type[7];
+            jaggedArray[2] = new Type[21];
+            Console.WriteLine(jaggedArray.Length);
+            Console.WriteLine(jaggedArray[1].Length);
+        }
+
+        public static void ArrayDemo()
+        {
+            int[] array1 = new int[10];
+            int[] array2 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int number = array2[2];
+            for(int i = 0; i < array2.Length; i++)
+            {
+                number = array2[i];
+            }
+            foreach(int i in array2)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
--- AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/Program.cs
+++ AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/Program.cs
@@ -6,24 +6,8 @@ namespace DataStructures
     {
         static void Main(string[] args)
         {
-            int[] arrayName = new int[10];
-            int[] oldNumbers = { 1, 2, 3, 4, 5 };
-            int number = oldNumbers[2];
-
-            for(int i = 0; i < oldNumbers.Length; i++)
-            {
-                number = oldNumbers[i];
-            }
-
-            int[,] arrayNameTwo = new int[10, 10];
-            int value = arrayNameTwo[0, 0];
-            int value2 = arrayNameTwo[0, 1];
-            int value3 = arrayNameTwo[1, 0];
-
-            Type[][] jaggedArray = new Type[10][];
-            jaggedArray[0] = new Type[5];
-            jaggedArray[1] = new Type[7];
-            jaggedArray[2] = new Type[21];
+            Array.ArrayDemo();
+            Array.TypeArrayDemo();
 
 
         }
