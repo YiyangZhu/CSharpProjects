d04ae513af6c7367ad54cc48aab98c2a84db0258
Yiyang Zhu <ererandkeke@gmail.com>
2019-01-30 20:49:42 -0500

    binary search tree

--- AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/BinaryTree.cs
+++ AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/BinaryTree.cs
@@ -1,10 +1,113 @@
 ﻿using System;
 namespace DataStructures
 {
-    public class BinaryTree
+    public class BinarySearchTree
     {
-        public BinaryTree()
+        public Node root;
+        public BinarySearchTree()
         {
+
+        }
+
+        public Node Insert(Node nodeToInsert)
+        {
+            return InsertHelper(root, nodeToInsert);
+        }
+
+        Node InsertHelper(Node node, Node nodeToInsert)
+        {
+            if (root == null)
+            {
+                root = nodeToInsert;
+                return root;
+            }
+            else if (node == null)
+            {
+                node = nodeToInsert;
+                return node;
+            }
+            else if (node.value == nodeToInsert.value)
+            {
+                return node;
+            }
+            else if (node.value < nodeToInsert.value)
+            {
+                if(node.right == null)
+                {
+                    node.right = nodeToInsert;
+                    return node.right;
+                }
+                else
+                {
+                    return InsertHelper(node.right, nodeToInsert);
+                }
+
+            }
+            else
+            {
+                if (node.left == null)
+                {
+                    node.left = nodeToInsert;
+                    return node.left;
+                }
+                else
+                {
+                    return InsertHelper(node.left, nodeToInsert);
+                }
+            }
+        }
+
+        public Node Search(Node node, int v)
+        {
+            if(node == null)
+            {
+                return null;
+            }
+            else if (node.value == v)
+            {
+                return node;
+            }
+            else if (node.value < v)
+            {
+                return Search(node.right, v);
+            }
+            else
+            {
+                return Search(node.left, v);
+            }
+        }
+
+        public void InOrderDisplayTree()
+        {
+            InOrderDisplayTreeHelper(root);
+        }
+
+        void InOrderDisplayTreeHelper(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.Write(node.value + " ");
+            InOrderDisplayTreeHelper(node.left);
+            InOrderDisplayTreeHelper(node.right);
+        }
+    }
+
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node()
+        {
+
+        }
+
+        public Node(int v)
+        {
+            value = v;
         }
     }
 }
--- AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/Program.cs
+++ AlgorithmAndDataStructureInCSharp/DataStructures/DataStructures/Program.cs
@@ -15,6 +15,22 @@ namespace DataStructures
             ExtendStructDemo.ExtendStructDemo_();
             */
 
+            BinarySearchTree BST = new BinarySearchTree();
+            Node Node1 = new Node(5);
+            Node Node2 = new Node(3);
+            Node Node3 = new Node(6);
+            Node Node4 = new Node(2);
+            Node Node5 = new Node(4);
+            BST.Insert(Node1);
+            BST.Insert(Node2);
+            BST.Insert(Node3);
+            BST.Insert(Node4);
+            BST.Insert(Node5);
+            BST.InOrderDisplayTree();
+
+
+
+
 
         }
     }
