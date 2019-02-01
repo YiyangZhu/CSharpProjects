b70e2c9d4ba73078be4c0b221910ec3630a02ae2
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-01 11:22:42 -0500

    generics self assess

--- AlgorithmAndDataStructureInCSharp/StandardCollection/StandardCollection/GenericLabOne.cs
+++ AlgorithmAndDataStructureInCSharp/StandardCollection/StandardCollection/GenericLabOne.cs
@@ -18,6 +18,7 @@ namespace StandardCollection
             students.Add(stud1);
             students.Add(stud2);
             students.Add(stud3);
+            /*
             foreach(Student stud in students)
             {
                 Console.WriteLine("Student first name is " + stud.FirstName);
@@ -25,6 +26,26 @@ namespace StandardCollection
             Console.WriteLine("Before removing student3, student count is " + students.Count);
             students.Remove(stud3);
             Console.WriteLine("After removing student3, student count is " + students.Count);
+            students.Add(stud3);
+            */
+
+            Random random = new Random();
+            foreach(Student stud in students)
+            {
+                for(int i = 0; i < 5; i++)
+                {
+                    stud.GradeStack.Push(random.Next(40, 100));
+                }
+            }
+            foreach(Student stud in students)
+            {
+                Console.Write($"Student name is {stud.FirstName} {stud.LastName}, grades are ");
+                foreach(int i in stud.GradeStack)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine("");
+            }
         }
 
         public class Student
@@ -33,6 +54,7 @@ namespace StandardCollection
             public string LastName { get; set; }
             public int Age { get; set; }
             public string Program { get; set; }
+            public Stack<int> GradeStack = new Stack<int>();
 
             public Student(string first, string last, int age, string prog)
             {
