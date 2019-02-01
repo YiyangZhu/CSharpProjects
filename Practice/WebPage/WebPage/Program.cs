6acca9fb8106082bb070843b8d3eb66a3e7af259
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-01 09:17:13 -0500

    get web content using C#

---
+++ Practice/WebPage/WebPage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace WebPage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //way 1 - using WebRequest
+            WebRequest request = WebRequest.Create("http://www.google.com");
+            WebResponse response = request.GetResponse();
+            Stream data = response.GetResponseStream();
+            string html = String.Empty;
+            using (StreamReader sr = new StreamReader(data))
+            {
+                html = sr.ReadToEnd();
+                Console.WriteLine(html);
+            }
+
+
+            //way 2 - using WebClient
+            WebClient client = new WebClient();
+            string downloadString = client.DownloadString("http://www.gooogle.com");
+            Console.WriteLine(downloadString);
+
+        }
+    }
+}
