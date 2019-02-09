2886f65cf5c07a084d3fca2b4521ee0f80705862
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-09 13:44:50 -0500

    partial class

---
+++ C#InDepth/Chapter7/Chapter7/Example1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Chapter7
+{
+    public class Exampe1
+    {
+        public Exampe1()
+        {
+        }
+    }
+}
---
+++ C#InDepth/Chapter7/Chapter7/Example2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Chapter7
+{
+    partial class Example <TFirst, TSecond> : EventArgs, IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
