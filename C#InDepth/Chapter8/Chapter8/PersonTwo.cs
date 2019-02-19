fd09ca1895f9e621e7571e97463805716e303046
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-19 15:15:33 -0500

    populate an array using anonymous types

--- C#InDepth/Chapter8/Chapter8/PersonTwo.cs
+++ C#InDepth/Chapter8/Chapter8/PersonTwo.cs
@@ -91,6 +91,31 @@ namespace Chapter8
 
         }
 
+        public static void DemoTwo()
+        {
+            var tom = new { Name = "Tom", Age = 9 };
+            var holly = new { Name = "Holly", Age = 36 };
+            var jon = new { Name = "Jon", Age = 36 };
+
+            Console.WriteLine("{0} is {1} years old", jon.Name, jon.Age);
+
+            var family = new[]
+            {
+                new { Name = "Holly", Age = 36 },
+                new { Name = "Jon", Age = 36 },
+                new { Name = "Tom", Age = 9 },
+                new { Name = "Robin", Age = 6 },
+                new { Name = "William", Age = 6 }
+            };
+
+            int totalAge = 0;
+            foreach(var person in family)
+            {
+                totalAge += person.Age;
+            }
+            Console.WriteLine("Total age: {0}", totalAge);
+        }
+
         public class Location
         {
             public string Country { get; set; }
--- C#InDepth/Chapter8/Chapter8/Program.cs
+++ C#InDepth/Chapter8/Chapter8/Program.cs
@@ -6,7 +6,7 @@ namespace Chapter8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PersonTwo.DemoTwo();
         }
     }
 }
