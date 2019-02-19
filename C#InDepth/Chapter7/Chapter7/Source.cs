f90a195f3d78df42b906f9bef6c5c25189a98477
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-19 14:30:52 -0500

    friend assemblies

---
+++ C#InDepth/Chapter7/Chapter7/Source.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+[assembly:InternalsVisibleTo("FriendAssembly")]
+
+namespace Chapter7
+{
+    public class Source
+    {
+        internal static void InternalMethod() { }
+        public static void PublicMethod() { }
+    }
+
+    public class Friend
+    {
+        static void Demo()
+        {
+            Source.InternalMethod();
+            Source.PublicMethod();
+        }
+    }
+
+    public class Enemy
+    {
+        static void Demo()
+        {
+            Source.PublicMethod();
+        }
+    }
+}
