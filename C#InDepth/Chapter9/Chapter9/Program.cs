d0a22999ff5485f361f16ab4d59bb29fa559e250
Yiyang Zhu <ererandkeke@gmail.com>
2019-02-19 15:35:04 -0500

    lambda expressions

---
+++ C#InDepth/Chapter9/Chapter9/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Chapter9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Demo();
+
+            Demo2();
+            */
+            Demo3();
+
+        }
+
+        public static void Demo()
+        {
+            Func<string, int> returnLength;
+            returnLength = delegate (string text) { return text.Length; };
+
+            Console.WriteLine(returnLength("Hello World"));
+        }
+
+        public static void Demo2()
+        {
+            Func<string, int> returnLength;
+            returnLength = (string text) => { return text.Length; };
+
+            Console.WriteLine(returnLength("Hello YY"));
+        }
+
+        public static void Demo3()
+        {
+            Func<string, int> returnLength;
+            returnLength = text => text.Length;
+
+            Console.WriteLine(returnLength("Hello Youngkey"));
+        }
+    }
+}
