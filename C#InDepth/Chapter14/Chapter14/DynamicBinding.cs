0fb0ec5cef47bbd1f070635dffb3ee4bb6d73712
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-05 11:51:57 -0500

    dynamic binding

---
+++ C#InDepth/Chapter14/Chapter14/DynamicBinding.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chapter14
+{
+    public class DynamicBinding
+    {
+        public DynamicBinding()
+        {
+        }
+
+        public static void Demo()
+        {
+            dynamic items = new List<string> { "First", "Second", "Third" };
+            dynamic valueToAdd = "!";
+            foreach(dynamic item in items)
+            {
+                string result = item + valueToAdd;
+                Console.WriteLine(result);
+            }
+        }
+
+        public static void Demo2()
+        {
+            dynamic items = new List<string> { "First", "Second", "Third" };
+            dynamic valueToAdd = 2;
+            foreach(dynamic item in items)
+            {
+                string result = item + valueToAdd;
+                Console.WriteLine(result);
+            }
+        }
+
+        public static void Demo3()
+        {
+            dynamic items = new List<int> { 1, 2, 3 };
+            dynamic valueToAdd = 2;
+            foreach (dynamic item in items)
+            {
+                //RuntimeBinderException: cannot implicitly convert type int to string
+                //string result = item + valueToAdd;
+                Console.WriteLine(item + valueToAdd);
+            }
+        }
+    }
+}
---
+++ C#InDepth/Chapter14/Chapter14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chapter14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            DynamicBinding.Demo();
+
+            DynamicBinding.Demo2();
+            */
+            DynamicBinding.Demo3();
+        }
+    }
+}
