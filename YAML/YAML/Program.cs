c6d0c4f9502bd53da1e21423586777d07c29cb97
Yiyang Zhu <ererandkeke@gmail.com>
2019-03-11 20:23:36 -0400

    yaml data structure demo

---
+++ YAML/YAML/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YAML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            YamlInterpreter y = new YamlInterpreter();
+            y.Demo();
+        }
+    }
+}
